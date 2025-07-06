4a69fb65a10c83cb2796fae05f0897baebd2193c
ograndegabriel <146639120+ograndegabriel@users.noreply.github.com>
2025-07-06 14:32:46 -0300

    este código apresenta falha na função PROMPT, fazendo o resto do codigo nao funcionar

---
+++ projetos.js
@@ -0,0 +1,4 @@
+const nomePrimeiro = prompt("escreva aqui: ");
+const nomeSegundo = prompt("qual é seu segundo nome?: ");
+const nomeCompleto = nomePrimeiro + " " + nomeSegundo;
+console.log(`Então seu nome é ${nomeCompleto}`);
